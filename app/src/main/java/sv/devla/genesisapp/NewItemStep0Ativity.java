09a0c4581345f86fba0d01b624922dbc96e44675
LUIS MENDOZA <lmendoza.sv@gmail.com>
2016-11-26 08:26:07 -0600

    sabado

--- app/src/main/java/sv/devla/genesisapp/NewItemStep0Ativity.java
+++ app/src/main/java/sv/devla/genesisapp/NewItemStep0Ativity.java
@@ -1,7 +1,12 @@
 package sv.devla.genesisapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -20,9 +25,26 @@ public class NewItemStep0Ativity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //inicio
-                Intent i = new Intent(NewItemStep0Ativity.this, ScanCodeBarActivity.class);
-                startActivity(i);
-                finish();
+
+if(isNetworkAvailable()){
+    Intent i = new Intent(NewItemStep0Ativity.this, ScanCodeBarActivity.class);
+    startActivity(i);
+    finish();
+}
+                else {
+    new AlertDialog.Builder(NewItemStep0Ativity.this)
+            .setTitle("No hay internet")
+            .setMessage("No hay una conexión de internet estable, por favor solucione el problema de internet e intente de nuevo")
+            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // continue with delete
+                }
+            })
+
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show();
+}
+
 
                 //fin
 
@@ -30,5 +52,11 @@ public class NewItemStep0Ativity extends AppCompatActivity {
         });
         //new IntentIntegrator(this).initiateScan();
 
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
