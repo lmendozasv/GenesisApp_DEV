5bcd3b1576786d449c72e00395f42823e0595a80
Luis Mendoza <lmendoza.sv@gmail.com>
2016-11-28 11:10:51 -0600

    28/09/16 11:05 AM LMendoza

---
+++ app/src/main/java/sv/devla/genesisapp/AddItemsFunction/NewItemStep0Ativity.java
@@ -0,0 +1,96 @@
+package sv.devla.genesisapp.AddItemsFunction;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import sv.devla.genesisapp.R;
+
+public class NewItemStep0Ativity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_item_step0_ativity);
+        this.setTitle("Nuevo artículo en Almacén");
+
+
+        Button btn1 = (Button) this.findViewById(R.id.btnsi);
+        Button btn2 = (Button) this.findViewById(R.id.btnno);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //inicio
+
+if(isNetworkAvailable()){
+    Intent i = new Intent(NewItemStep0Ativity.this, ScanCodeBarActivity.class);
+    startActivity(i);
+    finish();
+}
+                else {
+    new AlertDialog.Builder(NewItemStep0Ativity.this)
+            .setTitle("No hay internet")
+            .setMessage("No hay una conexión de internet estable, por favor solucione el problema de internet e intente de nuevo")
+            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // continue with delete
+                }
+            })
+
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .show();
+}
+
+
+                //fin
+
+            }
+        });
+        //new IntentIntegrator(this).initiateScan();
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //inicio
+
+                if(isNetworkAvailable()){
+                    Intent i = new Intent(NewItemStep0Ativity.this, NewItemStep1Activity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else {
+                    new AlertDialog.Builder(NewItemStep0Ativity.this)
+                            .setTitle("No hay internet")
+                            .setMessage("No hay una conexión de internet estable, por favor solucione el problema de internet e intente de nuevo")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // continue with delete
+                                }
+                            })
+
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+
+
+                //fin
+
+            }
+        });
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
