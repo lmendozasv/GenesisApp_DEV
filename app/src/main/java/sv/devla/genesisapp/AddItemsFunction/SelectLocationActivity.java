2fc4f3846228eb4d03a0cafe8db6e6e158551c15
Luis Mendoza <lmendoza.sv@gmail.com>
2016-12-05 15:35:32 -0600

    Camino al primer release demo

---
+++ app/src/main/java/sv/devla/genesisapp/AddItemsFunction/SelectLocationActivity.java
@@ -0,0 +1,31 @@
+package sv.devla.genesisapp.AddItemsFunction;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import sv.devla.genesisapp.R;
+
+public class SelectLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_location);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
