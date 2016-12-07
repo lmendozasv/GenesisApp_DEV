9350087afcb0da10677b4d6115992c0da0f043ce
LUIS MENDOZA <lmendoza.sv@gmail.com>
2016-12-07 04:09:16 -0600

    Agregado tipos de oferta

--- app/src/main/java/sv/devla/genesisapp/Reports/SalesByDeptoActivity.java
+++ app/src/main/java/sv/devla/genesisapp/Reports/SalesByDeptoActivity.java
@@ -1,13 +1,10 @@
 package sv.devla.genesisapp.Reports;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import org.eazegraph.lib.charts.BarChart;
-import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.BarModel;
-import org.eazegraph.lib.models.PieModel;
 
 import sv.devla.genesisapp.R;
 
@@ -18,15 +15,17 @@ public class SalesByDeptoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_by_depto);
 
-        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+        BarChart mBarChart = (BarChart) findViewById(R.id.barchart);
 
-        mPieChart.addPieSlice(new PieModel("Ellas", 15, Color.parseColor("#FE6DA8")));
-        mPieChart.addPieSlice(new PieModel("Ellos", 25, Color.parseColor("#56B7F1")));
-        mPieChart.addPieSlice(new PieModel("Baby & Kids", 35, Color.parseColor("#CDA67F")));
-        mPieChart.addPieSlice(new PieModel("Hogar", 9, Color.parseColor("#FED70E")));
-        mPieChart.addPieSlice(new PieModel("Otro 1", 5, Color.parseColor("#000000")));
-        mPieChart.addPieSlice(new PieModel("Otro 2", 11, Color.parseColor("#ffffff")));
+        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
+        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
+        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
+        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
+        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
+        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
 
-        mPieChart.startAnimation();
+        mBarChart.startAnimation();
     }
 }
